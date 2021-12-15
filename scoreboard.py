1626fb2fb4e82be2716351ee4720bb4c2ad5c9ca
BenBoekema <91763382+BenBoekema@users.noreply.github.com>
2021-12-14 20:42:22 -0400

    Add files via upload

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,10 @@
 # Author Ben Boekema (A00426722) and Srna
-# This program reads in 2 text files that
+# This program reads in a text files that
 # will later be used to create a json object
 # for the scoreboards data
 
-# import json for json implementation and os for checking if file is empty
+# import json for json implementation
 import json
-import os
 
 
 # Author Srna and Ben Boekema
@@ -62,74 +61,42 @@ def readWrite(uploadScore):
 def get_json():
     # file to be later opened and used for making json
     filename = 'scoreboard.txt'
-    filename2 = '../../public_html/scores.txt'
+    # resultant dictionary
+    dict1 = {}
 
-    # f2 is a file that will act as the placeholder for data that has not been implemented to the scoreboard
-    # this file can only be read from
-    f1 = open('../../public_html/scores.txt', 'r+')
-    # check to see if the file is empty, if empty close file
-    if os.stat('../../public_html/scores.txt').st_size == 0:
-        f1.close()
-    # if file is not empty continue
-    else:
-        # resultant dictionary
-        dict1 = {}
+    # fields for the scoreboard
+    fields = ['name', 'score']
 
-        # fields for the scoreboard
-        fields = ['name', 'score']
+    # open file to be converted to json
+    with open(filename) as fh:
+        # count variable for player id creation
+        l = 1
+        # for loop to iterate through lines in filename2
+        for line in fh:
 
-        # open file to be converted to json
-        with open(filename2) as fh:
-            # count variable for player id creation
-            l = 1
-            # for loop to iterate through lines in filename2
-            for line in fh:
+            # reading line by line from the text file
+            description = list(line.strip().split(":", 2))
 
-                # reading line by line from the text file
-                description = list(line.strip().split(":", 2))
+            # for automatic creation of id for each player
+            sno = 'id' + str(l)
 
-                readWrite(description)
+            # loop variable
+            i = 0
+            # intermediate dictionary
+            dict2 = {}
+            while i < len(fields):
+                # creating dictionary for each player
+                dict2[fields[i]] = description[i]
+                i = i + 1
 
-        with open(filename) as fh:
-            # count variable for player id creation
-            l = 1
-            # for loop to iterate through lines in filename2
-            for line in fh:
+            # appending the record of each player to
+            # the main dictionary
+            dict1[sno] = dict2
+            l = l + 1
 
-                # reading line by line from the text file
-                description = list(line.strip().split(":", 2))
-
-                # for automatic creation of id for each player
-                sno = 'id' + str(l)
-
-                # loop variable
-                i = 0
-                # intermediate dictionary
-                dict2 = {}
-                while i < len(fields):
-                    # creating dictionary for each player
-                    dict2[fields[i]] = description[i]
-                    i = i + 1
-
-                # appending the record of each player to
-                # the main dictionary
-                dict1[sno] = dict2
-                l = l + 1
-        # return f1 index to the start of the file
-        f1.seek(0)
-        # delete every line in the text file
-        f1.truncate(0)
-        # close file
-        f1.close()
-        # f1w is just another name for f2 except with write privileges
-        f1w = open("../../public_html/scores.txt", "w")
-        # write a empty statement so the text file always has a line of empty so it doesn't crash the
-        # program when run with no new data in scores.txt
-        f1w.write("Empty:-1")
-        # close files
-        f1w.close
-        # returns a json object of dict1
-        return json.JSONEncoder().encode(dict1)
+    # returns a json object of dict1
+    # this is a line to uncomment for testing purposes print(json.JSONEncoder().encode(dict1))
+    return json.JSONEncoder().encode(dict1)
 
 
 
